7a2922ea5148eab2317316cf45de836cf66525e6
goncaloarnauth <goncaloarnauth@outlook.com>
2026-01-06 13:01:08 +0000

    Refactor UI components for consistency and readability

--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -1,24 +1,23 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
   React.ComponentProps<"textarea">
 >(({ className, ...props }, ref) => {
   return (
-      <textarea
-          className={cn(
-            "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            // "focus-visible:outline-2 focus-visible:outline-ring focus-visible:outline-offset-0",
-            "focus-visible:outline-2 focus-visible:outline-border focus-visible:outline-offset-0",
-            className
-          )}
-        ref={ref}
-        {...props}
-      />
-  )
-})
-Textarea.displayName = "Textarea"
+    <textarea
+      className={cn(
+        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:outline-2 focus-visible:outline-border focus-visible:outline-offset-0",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Textarea.displayName = "Textarea";
 
-export { Textarea }
+export { Textarea };
